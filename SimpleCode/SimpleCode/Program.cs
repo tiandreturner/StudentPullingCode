2ab4d21c299e90953c52fe502cd930557e6dc75d
tjtics <tjtics@rit.edu>
2021-10-22 15:30:01 -0400

    Fixed and uploaded the right approach to teach student

---
+++ SimpleCode/SimpleCode/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
